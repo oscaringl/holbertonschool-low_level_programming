3c3099c5ce352fb2943593c6320cb6a4483b0924
Oscar Guerra <oogl29@gmail.com>
2022-10-06 05:34:24 -0500

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
-int main (void)
+int main(void)
 {
-  puts("\n""Programming is like building a multilingual puzzle");
+  puts("\n""Programming is like building a multilingual puzzle);
   return(0);
 }
