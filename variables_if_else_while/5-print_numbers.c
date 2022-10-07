ba83641a12aa0dca2f98fd76f5788e85004488bd
Oscar Guerra <oogl29@gmail.com>
2022-10-07 01:43:10 -0500

    Create c file 5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit = 0;
+
+	while (digit < 10)
+	{
+		printf("%d", digit++);
+	printf("\n");
+	return (0);
+}
