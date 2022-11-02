c29bc519398695e94094a5ced2fc9f8dddae4a0d
Oscar Guerra <oogl29@gmail.com>
2022-11-02 01:47:03 -0500

    Create c file 2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - function that prints 10 times the alphabet, in lowercase
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase
  *
  * Return: Always 0 (Success)
  */
