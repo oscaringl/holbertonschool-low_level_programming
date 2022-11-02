ae5b8e9b64c16f3948bd9f01fe12231056d7f96e
Oscar Guerra <oogl29@gmail.com>
2022-11-02 15:31:21 -0500

    Create c file 6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * int _abs - function that computes the absolute value of an integer.
+ * @i: The number to be computed.
+ *
+ * Return: Always 0.
+ */
+int _abs(int x)
+{
+	if (x < 0)
+	{
+		return (x);
+	}
+	else
+	{
+		return (x * -1);
+	}
+}
