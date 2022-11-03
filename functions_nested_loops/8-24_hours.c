c93da9adf42108ae206370027caf8d83ad469855
Oscar Guerra <oogl29@gmail.com>
2022-11-02 21:33:08 -0500

    Create c file 8-24_hours.c

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -9,9 +9,9 @@ void jack_bauer(void)
 {
 	int j, b;
 
-	for (j = 0; j <24; j++)
+	for (j = 0; j < 24; j++)
 	{
-		for (b =0; b < 60; b++)
+		for (b = 0; b < 60; b++)
 		{
 			_putchar((j / 10) + '0');
 			_putchar((j % 10) + '0');
