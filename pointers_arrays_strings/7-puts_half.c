5c850fbcc72874afc8df5244d276f61467f94427
Oscar Guerra <oogl29@gmail.com>
2022-11-15 11:12:35 -0500

    Create a 7-puts_half.c

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -14,14 +14,13 @@ void puts_half(char *str)
 
 	while (str[i] != '\0')
 		i++;
-	i = i - 1;
 
 	if (i % 2 != 0)
 		j = (i - 1 ) / 2;
 	else
 		j = (i / 2);
 
-	while (str[i] != '\0')
+	while (str[j] != '\0')
 		j++;
 	{
 		_putchar(str[j]);
