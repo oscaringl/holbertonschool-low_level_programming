b050b08fd63a5995510a75a1a93b704ad8d9ba1f
Oscar Guerra <oogl29@gmail.com>
2022-11-17 14:10:52 -0500

    Create a 0-strcat.c

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings.
+ * @dest: destination string.
+ * @src: source string.
+ *
+ * Return: pointer to the resulting string dest.
+ */
+
+char *_strcat(char *dest, char *src);
+{
+	int a = 0,  z = 0;
+	int i;
+
+	while (dest[a] != '\0')
+		a++;
+
+	while (src[z] != '\0')
+		z++;
+
+	for (i = 0; i <= z; i++)
+		dest[i + z] = src[i];
+
+	return(dest);
+}
