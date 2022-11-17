a454ecb81c8d578d9bf1b9c74deb50a078f8bb4d
Oscar Guerra <oogl29@gmail.com>
2022-11-17 15:00:33 -0500

    Create a 1-strncat.c

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -14,12 +14,12 @@ char *_strncat(char *dest, char *src, int n)
 	int i = 0, j = 0;
 
 	while (dest[j] != '\0')
+	{
 		j++;
+	}
 
 	while (i < n && src[i] != '\0')
-	{
 		i++;
-	}
 	dest[j + i] = src[i];
 	dest[j + i] = '\0';
 
