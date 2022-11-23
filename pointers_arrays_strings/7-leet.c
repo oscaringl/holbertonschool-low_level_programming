d2b449f7e6570d9d41aa5c963c9db15fd322158a
Oscar Guerra <oogl29@gmail.com>
2022-11-23 18:11:50 -0500

    Create a 7-leet.c

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -9,8 +9,8 @@
 char *leet(char *str)
 {
 	int i, j;
-	char letters[] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
-	char numbers[] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+	char letters[11] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	char numbers[11] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
 
 	for (i = 0; str[i] != '\0'; i++)
 		for (j = 0; letters[j] != '\0'; j++)
