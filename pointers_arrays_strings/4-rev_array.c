0f9257cba93f21142c24397408c5fd80c908114a
Oscar Guerra <oogl29@gmail.com>
2022-11-23 14:11:31 -0500

    Create a 4-rev_array.c

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -9,19 +9,13 @@
  */
 void reverse_array(int *a, int n)
 {
-	int i = 0;
-	int tmp;
+	int i, tmp;
 
-	while (a[i] != '\0')
-		i++;
-	i = i -1;
-
-	while (i != n && i > n)
+	for (i = 0; i < n; i++)
 	{
+		n--;
 		tmp = a[i];
 		a[i] = a[n];
 		a[n] = tmp;
-		i--;
-		n++;
 	}
 }
