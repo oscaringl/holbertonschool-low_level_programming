d16d973abb0b2e2f93fe9140baa9b012f16d408f
Oscar Guerra <oogl29@gmail.com>
2022-11-23 13:54:46 -0500

    Create a 4-rev_array.c

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * reverse_array - function that reverses the content of an array of integers.
+ * @a: an array of integers.
+ * @n: the number of elements to swap.
+ *
+ * Return: noting.
+ */
+reverse_array(int *a, int n)
+{
+	int i = 0;
+	int tmp;
+
+	while (i < n)
+		i++;
+	{
+		n--;
+		tmp = a[i];
+		a[i] = a[n];
+		a[n] = tmp;
+	}
+}
