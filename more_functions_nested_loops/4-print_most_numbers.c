b66f4df54f181e804687af6c0eee881df74d431d
Oscar Guerra <oogl29@gmail.com>
2022-11-24 11:35:43 -0500

    Create a function 4-print_most_numbers.c

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -10,12 +10,14 @@ void print_most_numbers(void)
 	int i;
 
 	for (i = 48; i < 58; i++)
-		if (i == 2 && i == 4)
+		if (i == 2)
 		{
 			continue;
 		}
+	if (i == 4 )
 	{
-		_putchar(i);
+			continue;
 	}
+	_putchar(i);
 	_putchar('\n');
 }
