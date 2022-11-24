acec9b808859854f81cef76b94e996c937c769db
Oscar Guerra <oogl29@gmail.com>
2022-11-24 14:12:10 -0500

    Create a function 9-fizz_buzz.c

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -17,13 +17,11 @@ int main(void)
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz");
 		else if (i % 3 == 0)
-			printf("Fizz");
+			printf("Fizz ");
 		else if (i % 5 == 0)
-			printf("Buzz");
+			printf("Buzz ");
 		else
-			printf("%i", i);
-		if (i != 100)
-			printf(" ");
+			printf("%d", i);
 	}
 	printf("\n");
 	return (0);
