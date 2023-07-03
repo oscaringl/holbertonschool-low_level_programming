5c51234bc40bf8a5ce383d0eb012c2880a8b9974
Oscar Guerra <oogl29@gmail.com>
2023-07-03 15:44:02 -0500

    Create file 7-print_chessboard.c

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * print_chessboard - Prints the chessboard.
+ * @a: The chessboard array.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int r, c;
+
+	for (r = 0; r < 8; ++r)
+	{
+		for (c = 0; c < 8; ++c)
+			_putchar(a[r][c]);
+		_putchar('\n');
+	}
+}
