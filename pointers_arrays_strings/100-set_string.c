34fdd1f7f7ca0daea908aaea21be3ebc4414fc93
Oscar Guerra <oogl29@gmail.com>
2023-07-03 15:52:02 -0500

    Create file 100-set_string.c

---
+++ pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * set_string - Sets the value of a pointer to a char.
+ * @s: Pointer to the pointer to be modified.
+ * @to: Pointer to the char to set the pointer to.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
