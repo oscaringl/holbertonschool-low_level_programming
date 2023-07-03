fa979487e125b035bc208cb5a57f5c592e957a20
Oscar Guerra <oogl29@gmail.com>
2023-07-03 16:22:38 -0500

    Create file holberton.h and 0-puts_recursion.c

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - Prints a string, followed by a new line.
+ * @s: The string to be printed.
+ */
+void _puts_recursion(char *s)
+void _puts_recursion(char *s)
+{
+	if ('\0' == *s)
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
