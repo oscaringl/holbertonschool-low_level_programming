a0d3b6188cb63b400a09e7a8d00c3c55f18e59e8
Oscar Guerra <oogl29@gmail.com>
2023-07-03 16:49:19 -0500

    Create file 6-is_prime_number.c

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -7,13 +7,13 @@
  *
  * Return: 1 if the number is prime, 0 otherwise.
  */
-int check_prime(int n, int i)
+int is_prime_helper(int n, int i)
 {
 	if (i * i > n)
 		return (1);
 	else if (n % i == 0)
 		return (0);
-	return (check_prime(n, i + 1));
+	return (is_prime_helper(n, i + 1));
 }
 
 /**
@@ -27,5 +27,5 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	return (check_prime(n, 2));
+	return (is_prime_helper(n, 2));
 }
