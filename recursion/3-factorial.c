b73b8336ecc4a3e92eeabd61dc2a2f9234906b4a
Oscar Guerra <oogl29@gmail.com>
2023-07-03 16:36:48 -0500

    Create file 3-factorial.c

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -6,9 +6,9 @@
  */
 int factorial(int n)
 {
-	if (n < 0) // Check for negative number
+	if (n < 0)
 		return (-1);
-	if (n == 0) // Base case: factorial of 0 is 1
+	if (n == 0)
 		return (1);
-	return (n * factorial(n - 1));  // Recursively call with n-1
+	return (n * factorial(n - 1));
 }
