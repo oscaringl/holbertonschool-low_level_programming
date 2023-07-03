5389590e9f4bd3eb992fde4f185c88faacb4f5bf
Oscar Guerra <oogl29@gmail.com>
2023-07-03 15:29:18 -0500

    Create file 2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -8,13 +8,13 @@
  */
 char *_strchr(char *s, char c)
 {
-    while (*s != '\0')
-    {
-        if (*s == c)
-            return s;
-        s++;
-    }
-    if (c == '\0')
-        return s;
-    return (0);
+	while (*s)
+	{
+		if (*s == c)
+			return (s);
+		++s;
+	}
+	if (*s == c)
+		return (s);
+	return (0);
 }
