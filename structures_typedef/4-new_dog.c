b174b8416c09214dc1e099a519467ad2d7f421d5
Oscar Guerra <oogl29@gmail.com>
2023-07-04 13:08:41 -0500

    Create file 4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -16,18 +16,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *name_copy, *owner_copy;
 	int name_len, owner_len;
 
-	/* Calculate the lengths of the name and owner strings */
 	for (name_len = 0; name[name_len]; name_len++)
 		;
 	for (owner_len = 0; owner[owner_len]; owner_len++)
 		;
 
-	/* Allocate memory for the new dog */
 	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
 
-	/* Allocate memory for the name and owner copies */
 	name_copy = malloc(sizeof(char) * (name_len + 1));
 	if (name_copy == NULL)
 	{
@@ -43,11 +40,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	/* Copy the name and owner strings */
 	strcpy(name_copy, name);
 	strcpy(owner_copy, owner);
 
-	/* Set the values of the new dog */
 	new_dog->name = name_copy;
 	new_dog->age = age;
 	new_dog->owner = owner_copy;
