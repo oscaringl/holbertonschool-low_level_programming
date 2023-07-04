e5ec22814399bfc64c9da34b73f6956d8ea5fa45
Oscar Guerra <oogl29@gmail.com>
2023-07-04 12:03:41 -0500

    Create file 101-mul.c

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -1,105 +1,126 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
-#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
 
 /**
- * is_valid_number - Checks if a string represents a valid number
- * @number: The string to be checked
+ * _is_zero - determines if any number is zero
+ * @argv: argument vector.
  *
- * Return: 1 if the string represents a valid number, 0 otherwise
- **/
+ * Return: no return.
+ */
+void _is_zero(char *argv[])
+{
+	int i, isn1 = 1, isn2 = 1;
 
-int is_valid_number(char *number)
+	for (i = 0; argv[1][i]; i++)
+		if (argv[1][i] != '0')
+		{
+			isn1 = 0;
+			break;
+		}
+
+	for (i = 0; argv[2][i]; i++)
+		if (argv[2][i] != '0')
+		{
+			isn2 = 0;
+			break;
+		}
+
+	if (isn1 == 1 || isn2 == 1)
+	{
+		printf("0\n");
+		exit(0);
+	}
+}
+
+/**
+ * _initialize_array - set memery to zero in a new array
+ * @ar: char array.
+ * @lar: length of the char array.
+ *
+ * Return: pointer of a char array.
+ */
+char *_initialize_array(char *ar, int lar)
 {
 	int i = 0;
 
-	while (number[i])
-	{
-		if (number[i] < '0' || number[i] > '9')
-			return 0;
-		i++;
-	}
-
-	return 1;
+	for (i = 0; i < lar; i++)
+		ar[i] = '0';
+	ar[lar] = '\0';
+	return (ar);
 }
 
 /**
- * multiply_numbers - Multiplies two positive numbers
- * @num1: The first number
- * @num2: The second number
+ * _checknum - determines length of the number
+ * and checks if number is in base 10.
+ * @argv: arguments vector.
+ * @n: row of the array.
  *
- * Return: 0 on success
+ * Return: length of the number.
  */
-int multiply_numbers(char *num1, char *num2)
+int _checknum(char *argv[], int n)
 {
-	int i, j, len1, len2, total_len;
-	int *result;
+	int ln;
 
-	len1 = strlen(num1);
-	len2 = strlen(num2);
-	total_len = len1 + len2;
-	result = calloc(total_len, sizeof(int));
-
-	if (!result)
-	{
-		printf("Error\n");
-		exit(98);
-	}
-
-	for (i = len1 - 1; i >= 0; i--)
-	{
-		for (j = len2 - 1; j >= 0; j--)
+	for (ln = 0; argv[n][ln]; ln++)
+		if (!isdigit(argv[n][ln]))
 		{
-			int mul = (num1[i] - '0') * (num2[j] - '0');
-			int p1 = i + j, p2 = i + j + 1;
-			int sum = mul + result[p2];
-
-			result[p1] += sum / 10;
-			result[p2] = sum % 10;
+			printf("Error\n");
+			exit(98);
 		}
-	}
 
-	i = 0;
-	while (i < total_len && result[i] == 0)
-		i++;
-
-	if (i == total_len)
-		printf("0\n");
-	else
-	{
-		while (i < total_len)
-			printf("%d", result[i++]);
-		printf("\n");
-	}
-
-	free(result);
-
-	return 0;
+	return (ln);
 }
 
 /**
- * main - Entry point
- * @argc: The number of command-line arguments
- * @argv: An array containing the command-line arguments
+ * main - Entry point.
+ * program that multiplies two positive numbers.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
  *
- * Return: 0 on success, 98 on error
+ * Return: 0 - success.
  */
 int main(int argc, char *argv[])
 {
+	int ln1, ln2, lnout, add, addl, i, j, k, ca;
+	char *nout;
+
 	if (argc != 3)
+		printf("Error\n"), exit(98);
+	ln1 = _checknum(argv, 1), ln2 = _checknum(argv, 2);
+	_is_zero(argv), lnout = ln1 + ln2, nout = malloc(lnout + 1);
+	if (nout == NULL)
+		printf("Error\n"), exit(98);
+	nout = _initialize_array(nout, lnout);
+	k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+	for (; k >= 0; k--, i--)
 	{
-		printf("Error\n");
-		return 98;
+		if (i < 0)
+		{
+			if (addl > 0)
+			{
+				add = (nout[k] - '0') + addl;
+				if (add > 9)
+					nout[k - 1] = (add / 10) + '0';
+				nout[k] = (add % 10) + '0';
+			}
+			i = ln1 - 1, j--, addl = 0, ca++, k = lnout - (1 + ca);
+		}
+		if (j < 0)
+		{
+			if (nout[0] != '0')
+				break;
+			lnout--;
+			free(nout), nout = malloc(lnout + 1), nout = _initialize_array(nout, lnout);
+			k = lnout - 1, i = ln1 - 1, j = ln2 - 1, ca = addl = 0;
+		}
+		if (j >= 0)
+		{
+			add = ((argv[1][i] - '0') * (argv[2][j] - '0')) + (nout[k] - '0') + addl;
+			addl = add / 10, nout[k] = (add % 10) + '0';
+		}
 	}
-
-	if (!is_valid_number(argv[1]) || !is_valid_number(argv[2]))
-	{
-		printf("Error\n");
-		return 98;
-	}
-
-	multiply_numbers(argv[1], argv[2]);
-
-	return 0;
+	printf("%s\n", nout);
+	return (0);
 }
