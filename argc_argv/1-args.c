747f87d62ede47ede0b5a6de9afbe2d9ff9a4ce4
Oscar Guerra <oogl29@gmail.com>
2023-07-03 21:49:22 -0500

    Create file 1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * @argc: Number of command line arguments
+ * @argv: Array containing the command line arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+    argv = argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
