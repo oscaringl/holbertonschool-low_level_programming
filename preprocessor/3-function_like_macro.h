ad3b8f28187732fcec9835725be1b887d560dfb7
Oscar Guerra <oogl29@gmail.com>
2023-07-04 12:36:52 -0500

    Create file 3-function_like_macro.h

---
+++ preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef _3_FUNCTION_LIKE_MACRO_H_
+#define _3_FUNCTION_LIKE_MACRO_H_
+
+/**
+ * ABS - Computes the absolute value of a number x.
+ * @x: The number to compute the absolute value of.
+ *
+ * Return: The absolute value of x.
+ */
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+
+#endif /* _3_FUNCTION_LIKE_MACRO_H_ */
