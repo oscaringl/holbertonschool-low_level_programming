27a439090dc1bebb5eb505f4e9ae7df6c1055c6b
Oscar Guerra <oogl29@gmail.com>
2023-07-04 13:35:43 -0500

    Create file 0-print_name.c

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -4,11 +4,10 @@
 /**
  * print_name - Prints a name.
  * @name: Name to be printed
- * @f: Pointer to the function that will be used to print the name
+ * @f: Pointer to a function that prints the name
  *
  * Return: void
- **/
-
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
