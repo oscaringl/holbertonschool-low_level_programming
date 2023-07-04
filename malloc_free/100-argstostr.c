2f62cfc58f7588eef397139b1281ae3dd0387ff2
Oscar Guerra <oogl29@gmail.com>
2023-07-03 22:35:47 -0500

    Create file 100-argstostr.c

---
+++ malloc_free/100-argstostr.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - Concatenates all the arguments of the program
+ * @ac: The argument count
+ * @av: Array of arguments
+ *
+ * Return: Pointer to a new string containing the concatenated arguments
+ *         or NULL if ac is 0 or av is NULL or if it fails
+ **/
+
+char *argstostr(int ac, char **av)
+{
+	char *strDup;
+	int i, j, k, size;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	size = 0;
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			size++;
+		size++;
+	}
+	size++;
+
+	strDup = malloc(sizeof(char) * size);
+	if (strDup == NULL)
+		return (NULL);
+	k = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			strDup[k++] = av[i][j];
+		}
+		strDup[k++] = '\n';
+	}
+	strDup[k] = '\0';
+	return (strDup);
+}
