fc02db070253e6e6278eba44c0d58658c443ac69
Oscar Guerra <oogl29@gmail.com>
2023-07-04 12:39:39 -0500

    Create file 4-sum.h

---
+++ preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef _4_SUM_H_
+#define _4_SUM_H_
+
+/**
+ * SUM - Computes the sum of two numbers x and y.
+ * @x: The first number.
+ * @y: The second number.
+ *
+ * Return: The sum of x and y.
+ */
+#define SUM(x, y) ((x) + (y))
+
+#endif /* _4_SUM_H_ */
