38afb8d42b43c761db274ab695bd4c8b7e6a3277
Oscar Guerra <oogl29@gmail.com>
2023-07-04 14:23:22 -0500

    Create file 3-calc.h

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -4,8 +4,8 @@
 /* Structure to hold operator and function */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
 /* Function prototypes */
