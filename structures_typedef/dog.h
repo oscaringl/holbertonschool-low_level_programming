864ed7d29da2c9f5cf765152ffbb5c0b01af5971
Oscar Guerra <oogl29@gmail.com>
2023-07-04 12:56:45 -0500

    Create file dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - Structure containing characteristics of a dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Name of the owner
+ *
+ * Description: This structure represents a dog and contains its name, age, and owner.
+ */
+typedef struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /* _DOG_H_ */
