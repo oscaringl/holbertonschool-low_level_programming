e5c1cc9f8d2adb4f48d8196fa3cdfd46c6b18136
Oscar Guerra <oogl29@gmail.com>
2023-07-04 19:57:44 -0500

    Create file 100-reverse_listint.c

--- more_singly_linked_lists/100-reverse_listint.c
+++ more_singly_linked_lists/100-reverse_listint.c
@@ -9,11 +9,10 @@ listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *prev = NULL;
 	listint_t *current = *head;
-	listint_t *next;
 
 	while (current != NULL)
 	{
-		next = current->next;
+		listint_t *next = current->next;
 		current->next = prev;
 		prev = current;
 		current = next;
