01a18e20b8a0d1aba9ba39083d3f5fae0004c7de
Oscar Guerra <oogl29@gmail.com>
2023-07-05 02:12:04 -0500

    Create file 5-get_dnodeint.c

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -15,5 +15,5 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	for (i = 0; current != NULL && i < index; i++)
 		current = current->next;
 
-	return current;
+	return (current);
 }
