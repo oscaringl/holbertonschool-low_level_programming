b60917442e83a8c17c384e7a6543cf74f0050e22
Oscar Guerra <oogl29@gmail.com>
2023-07-05 00:34:21 -0500

    Create 1-create_file.c

---
+++ file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * create_file - Creates a file and writes text content to it.
+ *
+ * @filename: The name of the file to create.
+ * @text_content: The text content to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, bytes_written, text_length = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		while (text_content[text_length])
+			text_length++;
+
+		bytes_written = write(fd, text_content, text_length);
+		if (bytes_written == -1 || bytes_written != text_length)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
