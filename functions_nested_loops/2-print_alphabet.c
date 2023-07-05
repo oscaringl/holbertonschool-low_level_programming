67ab387e6d96a157527b514319fe7f32bd1bed59
Oscar Guerra <oogl29@gmail.com>
2023-07-05 13:09:16 -0500

    Add 100-times_table.c 2-print_alphabet.c file

---
+++ functions_nested_loops/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - This program prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
