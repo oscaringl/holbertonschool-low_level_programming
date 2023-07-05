3f851dc975da3f9a5c60af6224004b0083535683
Oscar Guerra <oogl29@gmail.com>
2023-07-05 03:02:47 -0500

    Create file 0-hash_table_create.c

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,35 +1,27 @@
-#include <stdlib.h>
 #include "hash_tables.h"
 
 /**
  * hash_table_create - Creates a hash table.
  * @size: The size of the array.
  *
- * Return: A pointer to the newly created hash table,
- *         or NULL if something went wrong.
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new hash table.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *ht = NULL;
+	hash_table_t *ht;
+	unsigned long int i;
 
-    /* Allocate memory for the hash table */
-    ht = malloc(sizeof(hash_table_t));
-    if (ht == NULL)
-        return NULL;
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
 
-    /* Allocate memory for the array */
-    ht->array = malloc(sizeof(hash_node_t *) * size);
-    if (ht->array == NULL)
-    {
-        free(ht);
-        return NULL;
-    }
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
 
-    /* Initialize the size and array members */
-    ht->size = size;
-    /* Set all array elements to NULL */
-    for (unsigned long int i = 0; i < size; i++)
-        ht->array[i] = NULL;
-
-    return ht;
+	return (ht);
 }
