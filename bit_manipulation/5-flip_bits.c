771049a7258009bd0c27abcfbd56de7652188bf6
Oscar Guerra <oogl29@gmail.com>
2023-07-04 20:47:44 -0500

    Create file 5-flip_bits.c

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * flip_bits - Returns the number of bits to flip to get from one number to another.
+ * flip_bits - Returns the number of bits to flip to get from one
+ * number to another.
  * @n: The first number.
  * @m: The second number.
  *
@@ -12,7 +13,8 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int diff = n ^ m;
 	unsigned int count = 0;
 
-	while (diff > 0) {
+	while (diff > 0)
+	{
 		if (diff & 1)
 			count++;
 		diff >>= 1;
