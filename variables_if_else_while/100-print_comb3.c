6f5111b0ae4fd7d92832a17b82f0c693ea422d00
Oscar Guerra <oogl29@gmail.com>
2023-07-05 12:51:19 -0500

    Add 100-print_comb3.c file

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -7,24 +7,24 @@
  */
 int main(void)
 {
-	int i, j;
+    int i, j;
 
-	for (i = 0; i <= 8; i++)
-	{
-		for (j = i + 1; j <= 9; j++)
-		{
-			putchar((i % 10) + '0');
-			putchar((j % 10) + '0');
+    for (i = 0; i <= 8; i++)
+    {
+        for (j = i + 1; j <= 9; j++)
+        {
+            putchar((i % 10) + '0');
+            putchar((j % 10) + '0');
 
-			if (i != 8 || j != 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
+            if (i != 8 || j != 9)
+            {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
 
-	putchar('\n');
+    putchar('\n');
 
-	return (0);
+    return (0);
 }
