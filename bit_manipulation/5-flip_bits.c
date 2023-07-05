d03ed9c0e373236e405e1663d71812c8d3d27367
Oscar Guerra <oogl29@gmail.com>
2023-07-04 20:44:15 -0500

    Create file 5-flip_bits.c

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -12,8 +12,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int diff = n ^ m;
 	unsigned int count = 0;
 
-	while (diff > 0)
-	{
+	while (diff > 0) {
 		if (diff & 1)
 			count++;
 		diff >>= 1;
