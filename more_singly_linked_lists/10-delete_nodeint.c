af2e0af18428aad4e2741307e08d36ae80dd59ab
Oscar Guerra <oogl29@gmail.com>
2023-07-04 19:55:03 -0500

    Create file 10-delete_nodeint.c

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - Deletes the node at index index of a listint_t linked list
+ * delete_nodeint_at_index - Deletes the node at index index of a
+ * listint_t linked list
  * @head: Pointer to the head of the list
  * @index: Index of the node to be deleted
  * Return: 1 if it succeeded, -1 if it failed
