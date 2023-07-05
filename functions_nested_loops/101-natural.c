54faf1821135dde99a3e8ace1702d8eedad7bac9
Oscar Guerra <oogl29@gmail.com>
2023-07-05 13:16:45 -0500

    Add 100-times_table.c file

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -7,17 +7,17 @@
  */
 int main(void)
 {
-    int i, sum;
+	int i, sum;
 
-    sum = 0;
+	sum = 0;
 
-    for (i = 0; i < 1024; i++)
-    {
-        if (i % 3 == 0 || i % 5 == 0)
-            sum += i;
-    }
+	for (i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
+	}
 
-    printf("%d\n", sum);
+	printf("%d\n", sum);
 
-    return (0);
+	return (0);
 }
