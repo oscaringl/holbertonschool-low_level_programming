c97ce6cc447361be9b04e37d35be642d3f7c29c0
Oscar Guerra <oogl29@gmail.com>
2023-07-04 20:04:59 -0500

    Create file 102-free_listint_safe.c

--- more_singly_linked_lists/102-free_listint_safe.c
+++ more_singly_linked_lists/102-free_listint_safe.c
@@ -1,48 +1,96 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
+size_t looped_listint_count(listint_t *head);
+size_t free_listint_safe(listint_t **h);
+
 /**
- * free_listint_safe - Frees a listint_t linked list (safe version)
- * @h: Pointer to the head of the list
- * Return: Number of nodes freed
+ * looped_listint_count - Counts the number of unique nodes
+ *                      in a looped listint_t linked list.
+ * @head: A pointer to the head of the listint_t to check.
+ *
+ * Return: If the list is not looped - 0.
+ *         Otherwise - the number of unique nodes in the list.
+ */
+size_t looped_listint_count(listint_t *head)
+{
+	listint_t *tortoise, *hare;
+	size_t nodes = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	tortoise = head->next;
+	hare = (head->next)->next;
+
+	while (hare)
+	{
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+
+			tortoise = tortoise->next;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+			}
+
+			return (nodes);
+		}
+
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
+	}
+
+	return (0);
+}
+
+/**
+ * free_listint_safe - Frees a listint_t list safely (ie.
+ *                     can free lists containing loops)
+ * @h: A pointer to the address of
+ *     the head of the listint_t list.
+ *
+ * Return: The size of the list that was freed.
+ *
+ * Description: The function sets the head to NULL.
  */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *slow = *h, *fast = *h;
-	size_t count = 0;
-	int loop = 0;
+	listint_t *tmp;
+	size_t nodes, index;
 
-	if (*h == NULL)
-		return (0);
+	nodes = looped_listint_count(*h);
 
-	while (slow != NULL && fast != NULL && fast->next != NULL)
+	if (nodes == 0)
 	{
-		slow = slow->next;
-		fast = fast->next->next;
-
-		if (slow == fast)
+		for (; h != NULL && *h != NULL; nodes++)
 		{
-			loop = 1;
-			break;
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
 		}
 	}
 
-	if (loop)
+	else
 	{
-		printf("Loop detected, unable to free list\n");
-		exit(98);
+		for (index = 0; index < nodes; index++)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+		}
+
+		*h = NULL;
 	}
 
-	while (*h != NULL)
-	{
-		count++;
-		slow = *h;
-		*h = (*h)->next;
-		free(slow);
-	}
+	h = NULL;
 
-	*h = NULL;
-
-	return (count);
+	return (nodes);
 }
