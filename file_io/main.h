534145dfc2287735b0197ad23d9650bc7264af86
Oscar Guerra <oogl29@gmail.com>
2023-07-05 00:53:26 -0500

    Create file main.h

--- file_io/main.h
+++ file_io/main.h
@@ -12,5 +12,6 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int cp(const char *file_from, const char *file_to);
 int elf_header(const char *elf_filename);
+int _putchar(char c);
 
 #endif /* MAIN_H */
