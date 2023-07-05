0a51b76a5aa2581864422641414b4509d75ba103
Oscar Guerra <oogl29@gmail.com>
2023-07-05 13:12:30 -0500

    Add 100-times_table.c file

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,59 +1,46 @@
 #include "main.h"
 
 /**
- * print_number - Prints a number with a specified width
- * @num: The number to be printed
- * @width: The minimum width of the number
- *
- * Return: void
- */
-void print_number(int num, int width)
-{
-	if (num < 0)
-	{
-		_putchar('-');
-		num = -num;
-	}
-
-	if (width >= 100)
-		_putchar((num / 100) + '0');
-	if (width >= 10)
-		_putchar(((num / 10) % 10) + '0');
-
-	_putchar((num % 10) + '0');
-}
-
-/**
  * print_times_table - Prints the n times table
- * @n: The number of times to print the table
  *
- * Return: void
+ * @n: number times table (0 < n <= 15)
+ *
+ * Return: no return
  */
 void print_times_table(int n)
 {
-	int i, j, result;
+	int a, b, op;
 
-	if (n > 15 || n < 0)
-		return;
-
-	for (i = 0; i <= n; i++)
+	if (n >= 0 && n <= 15)
 	{
-		for (j = 0; j <= n; j++)
+		for (a = 0; a <= n; a++)
 		{
-			result = i * j;
-
-			if (j != 0)
-				_putchar(' ');
-
-			print_number(result, n * n);
-
-			if (j != n)
+			_putchar(48);
+			for (b = 1; b <= n; b++)
 			{
-				_putchar(',');
-				_putchar(' ');
+				op = a * b;
+				_putchar(44);
+				_putchar(32);
+				if (op <= 9)
+				{
+					_putchar(32);
+					_putchar(32);
+					_putchar(op + 48);
+				}
+				else if (op <= 99)
+				{
+					_putchar(32);
+					_putchar((op / 10) + 48);
+					_putchar((op % 10) + 48);
+				}
+				else
+				{
+					_putchar(((op / 100) % 10) + 48);
+					_putchar(((op / 10) % 10) + 48);
+					_putchar((op % 10) + 48);
+				}
 			}
+			_putchar('\n');
 		}
-
-		_putchar('\n');
 	}
 }
