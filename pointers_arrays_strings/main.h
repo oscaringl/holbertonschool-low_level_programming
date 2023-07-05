976f0d6ca8d42411ce8c8eada4a9a42bd038c18b
Oscar Guerra <oogl29@gmail.com>
2023-07-05 16:49:29 -0500

    Add main.h file

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -33,4 +33,10 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
+char *rot13(char *);
+void print_number(int n);
+int _putchar(char);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+
 #endif
